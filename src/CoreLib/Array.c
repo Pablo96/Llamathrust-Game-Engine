f5461e282e5868f670e1ca263dd03cb31dabcd73
Pablo Narvaja <pnarvaja.21@gmail.com>
2020-07-10 01:19:03 -0300

    CoreLib Implementation started

---
+++ src/CoreLib/Array.c
@@ -0,0 +1,25 @@
+#include <CoreLib/Array.h>
+
+Array LT_ArrayCreate(uint64 size, uint64 typeSize) {
+
+}
+
+Array LT_ArrayStackCreate(void* dataBuffer, uint64 size, uint64 typeSize) {
+
+}
+
+uint64 LT_ArrayCount(const Array* array) {
+
+}
+
+void LT_ArrayDestroy(const Array* array) {
+
+}
+
+void* LT_ArrayGetElement(const Array* array, const uint64 index) {
+
+}
+
+void LT_ArraySetElement(const Array* array, const uint64 index, void* element) {
+    
+}
