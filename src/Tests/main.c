1910889af8adb4e4e8a83bf7180609154ff02d90
Pablo Narvaja <pnarvaja.21@gmail.com>
2020-07-23 21:41:51 -0300

    Update main.c

--- src/Tests/main.c
+++ src/Tests/main.c
@@ -16,9 +16,9 @@ int main(int argc, const char** argv) {
     LT_ADD_TEST(TestThreadSpawn, TEST_SUCCESS);
     LT_ADD_TEST(TestThreadExitCode, TEST_SUCCESS);
     LT_ADD_TEST(TestThreadLock, TEST_SUCCESS);
-    
-    
     LT_ADD_TEST(TestThreadPool, TEST_SUCCESS);
+
+    
 
     return LT_TestRun();
 }
