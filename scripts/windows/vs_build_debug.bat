34f6ff48c9d4e59b4870dc4c98be5100942426b5
Pablo Narvaja <pnarvaja.21@gmail.com>
2020-11-22 01:27:04 -0300

    Win32 Tests and VS2019 build script

--- scripts/windows/vs_build_debug.bat
+++ scripts/windows/vs_build_debug.bat
@@ -1,23 +1,15 @@
 cls
-cd ../..
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat";
+if not exist "..\..\project_files" (
+    call "cmake_vs_proj.bat"
+    cd ..
+) else (
+    cd ../..
+)
 if not exist ".\bin\debug" mkdir ".\bin\debug"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat";
 
 :: Engine Executable
-cl /nologo /std:c++17 /GS  /W3 /Gd /MDd /fp:precise ^
-/D "WIN32" /D "_WINDOWS" /D "LT_DEBUG" /D "_MBCS" /D "_CRT_SECURE_NO_WARNINGS" ^
-/I".\include" ^
-/I".\external" ^
-/I".\external\log-c" ^
-/I".\external\glad\include" ^
-/Z7 /Od /Ob0 ^
-/TC ./external/glad/src/glad.c ./external/log-c/log.c ./src/engine/*.c ./src/engine/platform/Win32.c
-/link ^
-/DEBUG ^
-/SUBSYSTEM:CONSOLE ^
-/OUT:"D:\Workspace\Engines\LlamathrustC_V0.1\bin\debug\engine_vs.exe" ^
-/DYNAMICBASE "opengl32.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "ws2_32.lib"
+msbuild ./project_files/llamathrust.vcxproj -property:Configuration=Debug
 
-del *.obj
 del .\bin\debug\*.ilk
 del .\bin\debug\*.pdb
